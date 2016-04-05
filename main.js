43725690d40f893a0d3644f6dc799d56f29d8d21
AlexSolari <alexsanify@gmail.com>
2016-04-05 17:19:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ window.onload = function () {
     var n3 = graph.AddNode();    
     var n4 = graph.AddNode();    
     var n5 = graph.AddNode();
+    var n6 = graph.AddNode();    
+    var n7 = graph.AddNode();    
+    var n8 = graph.AddNode();
 
     graph.LinkNodes(n1, n4, false, false);
     graph.LinkNodes(n4, n3, false, false);
@@ -17,6 +20,10 @@ window.onload = function () {
     graph.LinkNodes(n3, n1, false, false);
     graph.LinkNodes(n3, n5, false, false);
     graph.LinkNodes(n4, n2, false, false);
+    graph.LinkNodes(n1, n7, false, false);
+    graph.LinkNodes(n8, n6, false, false);
+    graph.LinkNodes(n5, n6, false, false);
+    graph.LinkNodes(n8, n2, false, false);
     //graph.LinkNodes(n4, n4, false, false);
     
     graph.Draw(true);
