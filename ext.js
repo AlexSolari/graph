4f055c2b18193a877b0e1807c8211a2048e80aad
AlexSolari <alexsanify@gmail.com>
2016-04-05 20:22:25 +0300

    Update ext.js

--- ext.js
+++ ext.js
@@ -5,3 +5,20 @@ Array.prototype.first = function () {
 Array.prototype.last = function () {
     return this[this.length - 1];
 }
+
+Array.prototype.clear = function() {
+    while(this.length > 0)
+        this.pop();
+}
+
+Array.prototype.dequeue = function(){
+    var result = this.first();
+    
+    for (var index = 0; index < this.length; index++) {
+        this[index] = this[index + 1];
+    }
+    
+    this.pop();
+    
+    return result;
+}
